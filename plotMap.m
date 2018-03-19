748d3e1ab2a419f8578957042475154e48c18e9e
jwahlst <jwahlst@UG.KTH.SE>
2018-03-19 15:29:04 +0000

    Test

--- plotMap.m
+++ plotMap.m
@@ -1,10 +1,15 @@
-function [] = plotMap(map,currentFloor)
+% Function that plots floor plan.
+
+% map - floor plan data.
+% currentSections - The sections that will be highlighted in the plot.
+
+function [] = plotMap(map,currentSections)
 
     % Extract section names.
     sections = fieldnames(map.sections);
 
     % Define floors except currentFloor.
-    otherFloors = setdiff(1:length(sections),currentFloor);
+    otherFloors = setdiff(1:length(sections),currentSections);
 
     % Make plots of floors except currentFloor.
     for l1 = otherFloors 
@@ -12,17 +17,19 @@ function [] = plotMap(map,currentFloor)
         graphs = fieldnames(fl);
         for l2 = 1:length(graphs)
             temp = fl.(graphs{l2});
-            plot(temp(:,1)+map.dx,temp(:,2)+map.dy,'color',0.9*[1 1 1],'LineWidth',3)
+            plot(temp(:,1),temp(:,2),'color',0.9*[1 1 1],'LineWidth',3)
         end
     end
 
     % Make plots of currentFloor.
-    if(~isempty(currentFloor))
-        fl = map.sections.(sections{currentFloor});
-        graphs = fieldnames(fl);
-        for l2 = 1:length(graphs)
-            temp = fl.(graphs{l2});
-            plot(temp(:,1)+map.dx,temp(:,2)+map.dy,'color',0.2*[1 1 1],'LineWidth',3)
+    if(~isempty(currentSections))
+        for se = 1:length(currentSections)
+            fl = map.sections.(sections{currentSections(se)});
+            graphs = fieldnames(fl);
+            for l2 = 1:length(graphs)
+                temp = fl.(graphs{l2});
+                plot(temp(:,1),temp(:,2),'color',0.2*[1 1 1],'LineWidth',3)
+            end
         end
     end
     
@@ -30,7 +37,7 @@ function [] = plotMap(map,currentFloor)
     for l1 = 1:size(map.transitions,3)
         for l2 = l1+1:size(map.transitions,3)
             transition = map.transitions;
-            plot(transition(:,1,l1,l2)+map.dx,transition(:,2,l1,l2)+map.dy,'color',[1 0 0],'LineWidth',2)
+            plot(transition(:,1,l1,l2),transition(:,2,l1,l2),'color',[1 0 0],'LineWidth',2)
         end
     end
     
